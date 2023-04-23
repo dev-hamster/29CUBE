72b7c5654318c36df13ed4bf45c6401df02105c7
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-04-23 18:54:02 +0900

    style: 2 STEP 콘텐츠 디자인

---
+++ src/app/quiz/tmp/Keywords.tsx
@@ -0,0 +1,79 @@
+import Form from '@/app/components/Form';
+import Quiz from '@/app/components/Quiz';
+import style from './Keywords.module.scss';
+
+export default function Keywords() {
+  return (
+    <div>
+      <Quiz>
+        응삼이님은 <br />
+        어떤 사람이에요?
+      </Quiz>
+      <Form>
+        <label htmlFor=''>3가지 키워드를 선택해 주세요!</label>
+        <div className={style.container}>
+          <div className={style.group}>
+            <input type='checkbox' name='keywords' id='tmp' />
+            <label htmlFor='tmp'>냐냐냐</label>
+            <input type='checkbox' name='keywords' id='tmp2' />
+            <label htmlFor='tmp2' className={style.center}>
+              먀먀먀
+            </label>
+            <input type='checkbox' name='keywords' id='tmp3' />
+            <label htmlFor='tmp3'>라라라</label>
+          </div>
+          <div className={style.group}>
+            <input type='checkbox' name='keywords' id='tmp' />
+            <label htmlFor='tmp'>냐냐냐</label>
+            <input type='checkbox' name='keywords' id='tmp2' />
+            <label htmlFor='tmp2' className={style.center}>
+              먀먀먀
+            </label>
+            <input type='checkbox' name='keywords' id='tmp3' />
+            <label htmlFor='tmp3'>라라라</label>
+          </div>
+          <div className={style.group}>
+            <input type='checkbox' name='keywords' id='tmp' />
+            <label htmlFor='tmp'>냐냐냐</label>
+            <input type='checkbox' name='keywords' id='tmp2' />
+            <label htmlFor='tmp2' className={style.center}>
+              먀먀먀
+            </label>
+            <input type='checkbox' name='keywords' id='tmp3' />
+            <label htmlFor='tmp3'>라라라</label>
+          </div>
+          <div className={style.group}>
+            <input type='checkbox' name='keywords' id='tmp' />
+            <label htmlFor='tmp'>냐냐냐</label>
+            <input type='checkbox' name='keywords' id='tmp2' />
+            <label htmlFor='tmp2' className={style.center}>
+              먀먀먀
+            </label>
+            <input type='checkbox' name='keywords' id='tmp3' />
+            <label htmlFor='tmp3'>라라라</label>
+          </div>
+          <div className={style.group}>
+            <input type='checkbox' name='keywords' id='tmp' />
+            <label htmlFor='tmp'>냐냐냐</label>
+            <input type='checkbox' name='keywords' id='tmp2' />
+            <label htmlFor='tmp2' className={style.center}>
+              먀먀먀
+            </label>
+            <input type='checkbox' name='keywords' id='tmp3' />
+            <label htmlFor='tmp3'>라라라</label>
+          </div>
+          <div className={style.group}>
+            <input type='checkbox' name='keywords' id='tmp' />
+            <label htmlFor='tmp'>냐냐냐</label>
+            <input type='checkbox' name='keywords' id='tmp2' />
+            <label htmlFor='tmp2' className={style.center}>
+              먀먀먀
+            </label>
+            <input type='checkbox' name='keywords' id='tmp3' />
+            <label htmlFor='tmp3'>라라라</label>
+          </div>
+        </div>
+      </Form>
+    </div>
+  );
+}

e66fe0431af9b64e269f51d493f3cceaf0342b02
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-04-26 01:45:55 +0900

    feat: 프로세트 활성화

--- src/app/components/Process/index.tsx
+++ src/app/components/Process/index.tsx
@@ -1,15 +1,16 @@
 import style from './Process.module.css';
 
-export default function Process() {
+export default function Process({ step }: { step: number }) {
+  const processArr = [1, 2, 3, 4, 5, 6, 7];
+
   return (
     <div className={style.process}>
-      <div className={`${style.gauge} ${style.active}`} />
-      <div className={style.gauge} />
-      <div className={style.gauge} />
-      <div className={style.gauge} />
-      <div className={style.gauge} />
-      <div className={style.gauge} />
-      <div className={style.gauge} />
+      {processArr.map((step_) => (
+        <div
+          key={step_}
+          className={`${style.gauge} ${step_ === step ? style.active : ''}`}
+        />
+      ))}
     </div>
   );
 }

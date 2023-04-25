de46dae7ed9e207b70e5e61bb8803f3cec5df0a0
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-04-26 01:45:22 +0900

    style: 버튼 링크 스타일 추가

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,15 +1,44 @@
+'use client';
+
+import Link from 'next/link';
 import style from './Button.module.css';
 
 export default function Button({
   children,
+  isActive,
+  type,
+  handleClick,
+  href,
   theme,
 }: {
   children: string;
+  isActive?: boolean;
+  type?: 'link' | 'button';
+  handleClick?: () => void;
+  href?: string;
   theme?: 'black' | 'white';
 }) {
   theme = theme || 'black';
   const themeStyle = theme === 'black' ? style.black : '';
+
   return (
-    <button className={`${style.button} ${themeStyle}`}>{children}</button>
+    <>
+      {type !== 'link' && (
+        <button
+          className={`${style.button} ${themeStyle}`}
+          disabled={!isActive}
+          onClick={() => {
+            if (handleClick) handleClick();
+          }}
+        >
+          {children}
+        </button>
+      )}
+      {type === 'link' && (
+        <div className={`${style.button} ${themeStyle}`}>
+          <Link href={href + ''}>{children}</Link>
+        </div>
+      )}
+    </>
   );
 }

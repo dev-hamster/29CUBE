0d7b0e795af4dc6a9924a9d8202e51598ffbc8f4
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-04-26 01:43:01 +0900

    feat: 테스트 라우팅
    - 뒤로가기, 다음 버튼 라우팅 구현

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,5 +1,114 @@
-import Age from './tmp/Gender';
+'use client';
 
-export default function Page() {
-  return <Age />;
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import Button from '@/app/components/Button';
+import Process from '@/app/components/Process';
+import Age from '@/app/components/Content/Age';
+import Name from '@/app/components/Content/Name';
+import Gender from '@/app/components/Content/Gender';
+import Keywords from '@/app/components/Content/Keywords';
+import Place from '@/app/components/Content/Place';
+import Color from '@/app/components/Content/Color';
+import Stuff from '@/app/components/Content/Stuff';
+import Style from '@/app/components/Content/Style';
+import Texture from '@/app/components/Content/Texture';
+import './layout.css';
+
+export default function QuizLayout() {
+  const router = useRouter();
+  const [step, setStep] = useState(0);
+  const [keysIdx, setKeyIdx] = useState(0);
+  const { name, keys } = quizContents[step];
+
+  const [nickname, setNickname] = useState('');
+  const [gender, setGender] = useState('');
+
+  const Contents = {
+    Name: <Name handleChange={() => {}} />,
+    Age: <Age />,
+    Gender: <Gender />,
+    Keywords: <Keywords />,
+    Place: <Place />,
+    Color: <Color />,
+    Texture: <Texture />,
+    Stuff: <Stuff />,
+    Style: <Style />,
+  };
+
+  const handleClick = () => {
+    if (keysIdx == keys.length - 1) {
+      if (step == quizContents.length - 1) {
+        router.push('/result');
+        return;
+      }
+      setStep((prev) => prev + 1);
+      setKeyIdx(0);
+      return;
+    }
+    setKeyIdx((prev) => prev + 1);
+  };
+
+  const handleBack = () => {
+    if (step == 0) {
+      setKeyIdx((prev) => prev - 1);
+      return;
+    }
+    if (step - 1 == 0) {
+      setStep((prev) => prev - 1);
+      setKeyIdx(quizContents[0].keys.length - 1);
+      return;
+    }
+    setStep((prev) => prev - 1);
+    setKeyIdx(0);
+  };
+
+  return (
+    <main>
+      <header>
+        <div className='process-container'>
+          <Process step={step + 1} />
+        </div>
+        <div className='quiz-nav'>
+          {/* 첫 페이지는 뒤로가기 버튼 노출 X */}
+          {!(step === 0 && keysIdx === 0) && (
+            <span className='go-back' onClick={handleBack}>
+              <Image
+                src='/images/left-arrow.svg'
+                width={8}
+                height={15}
+                alt=''
+              />
+            </span>
+          )}
+          <p className='name'>{name}</p>
+        </div>
+      </header>
+      {Contents[keys[keysIdx] as keyof typeof Contents]}
+      <div className='next-step'>
+        <Button handleClick={handleClick} isActive>
+          다음
+        </Button>
+      </div>
+    </main>
+  );
 }
+
+const quizContents = [
+  {
+    step: 1,
+    name: 'STEP 1',
+    keys: ['Name', 'Age', 'Gender'],
+  },
+  {
+    step: 2,
+    name: 'STEP 2',
+    keys: ['Keywords'],
+  },
+  { name: 'STEP 3', keys: ['Place'] },
+  { name: 'STEP 4', keys: ['Color'] },
+  { name: 'STEP 5', keys: ['Texture'] },
+  { name: 'STEP 6', keys: ['Stuff'] },
+  { name: 'STEP 7', keys: ['Style'] },
+];

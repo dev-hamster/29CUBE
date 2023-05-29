a7487e478bff4e391196101c4849b3d3d422effd
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-05-30 02:58:18 +0900

    sylte: 코드 스타일 수정

--- src/app/result/[...slug]/page.tsx
+++ src/app/result/[...slug]/page.tsx
@@ -1,17 +1,12 @@
 'use client';
 
-import React, { ReactNode, use, useEffect, useState } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import html2canvas from 'html2canvas';
 import Button from '@/app/components/Button';
 import './page.scss';
 import { fetchResult } from '@/api';
-
-// TODO 토스트 메세지 이상함
-// TODO 캡쳐 영역 이상함 아 제발
-// TODO 다시하기 전역 상태 초기화
-// TODO api 연동
 
 const screenshotId = 'screenshot';
 
@@ -145,209 +140,211 @@ export default function Page({ params }: { params: { slug: string[] } }) {
           <Image src='/images/save.svg' fill alt='' />
         </button>
       </div>
-      <div className='title'>
-        <p>{nickname}님의 큐브는</p>
-        <h1>{data.survey_result_user_type_kr}</h1>
-      </div>
-      <div className='cube'>
-        <Image src={CUBE_URL} fill alt='' />
-      </div>
-      <div className='en_title'>
-        <div className='box'>{data.survey_result_user_type_en}</div>
-      </div>
-      <div className='desc'>
-        <div className='box'>{data.survey_result_user_type_description}</div>
-      </div>
-      <div className='figure-container'>
-        <p>큐브 전개도</p>
-        <div className='box'>
-          <div className='figure'>
-            <div className='block'>
-              <div className='text left'>
-                <a
-                  href={data.cube_expansion[0].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <p className='brand'>
-                    {data.cube_expansion[0].cube_expansion_image_name}
-                  </p>
-                  <p className='product'>
-                    {data.cube_expansion[0].cube_expansion_image_description}
-                  </p>
-                </a>
-              </div>
-              <div className='image'>
-                <a
-                  href={data.cube_expansion[0].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <span className='idx'>01</span>
-                  <Image src={CUBE_EXTENSTION_URL(1)} fill alt='' />
-                </a>
-              </div>
-              <div className='text right'>
-                <a
-                  href={data.cube_expansion[1].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <p className='brand'>
-                    {data.cube_expansion[1].cube_expansion_image_name}
-                  </p>
-                  <p className='product'>
-                    {data.cube_expansion[1].cube_expansion_image_description}
-                  </p>
-                </a>
+
+      <>
+        <div className='title'>
+          <p>{nickname}님의 큐브는</p>
+          <h1>{data.survey_result_user_type_kr}</h1>
+        </div>
+        <div className='cube'>
+          <Image src={CUBE_URL} fill alt='' />
+        </div>
+        <div className='en_title'>
+          <div className='box'>{data.survey_result_user_type_en}</div>
+        </div>
+        <div className='desc'>
+          <div className='box'>{data.survey_result_user_type_description}</div>
+        </div>
+        <div className='figure-container'>
+          <p>큐브 전개도</p>
+          <div className='box'>
+            <div className='figure'>
+              <div className='block'>
+                <div className='text left'>
+                  <a
+                    href={data.cube_expansion[0].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <p className='brand'>
+                      {data.cube_expansion[0].cube_expansion_image_name}
+                    </p>
+                    <p className='product'>
+                      {data.cube_expansion[0].cube_expansion_image_description}
+                    </p>
+                  </a>
+                </div>
+                <div className='image'>
+                  <a
+                    href={data.cube_expansion[0].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <span className='idx'>01</span>
+                    <Image src={CUBE_EXTENSTION_URL(1)} fill alt='' />
+                  </a>
+                </div>
+                <div className='text right'>
+                  <a
+                    href={data.cube_expansion[1].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <p className='brand'>
+                      {data.cube_expansion[1].cube_expansion_image_name}
+                    </p>
+                    <p className='product'>
+                      {data.cube_expansion[1].cube_expansion_image_description}
+                    </p>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='figure'>
-            <div className='block'>
-              <div className='text left'>
-                <a
-                  href={data.cube_expansion[2].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <p className='brand'>
-                    {data.cube_expansion[2].cube_expansion_image_name}
+            <div className='figure'>
+              <div className='block'>
+                <div className='text left'>
+                  <a
+                    href={data.cube_expansion[2].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <p className='brand'>
+                      {data.cube_expansion[2].cube_expansion_image_name}
+                    </p>
+                  </a>
+                  <p className='product'>
+                    {data.cube_expansion[2].cube_expansion_image_description}
                   </p>
-                </a>
-                <p className='product'>
-                  {data.cube_expansion[2].cube_expansion_image_description}
-                </p>
-              </div>
-              <div className='image border-top'>
-                <a
-                  href={data.cube_expansion[1].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <span className='idx'>02</span>
-                  <Image src={CUBE_EXTENSTION_URL(2)} fill alt='' />
-                </a>
-              </div>
-              <div className='image right border-left'>
-                <a
-                  href={data.cube_expansion[2].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <span className='idx'>03</span>
-                  <Image src={CUBE_EXTENSTION_URL(3)} fill alt='' />
-                </a>
+                </div>
+                <div className='image border-top'>
+                  <a
+                    href={data.cube_expansion[1].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <span className='idx'>02</span>
+                    <Image src={CUBE_EXTENSTION_URL(2)} fill alt='' />
+                  </a>
+                </div>
+                <div className='image right border-left'>
+                  <a
+                    href={data.cube_expansion[2].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <span className='idx'>03</span>
+                    <Image src={CUBE_EXTENSTION_URL(3)} fill alt='' />
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='figure'>
-            <div className='block'>
-              <div className='image left'>
-                <a
-                  href={data.cube_expansion[3].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <span className='idx'>04</span>
-                  <Image src={CUBE_EXTENSTION_URL(4)} fill alt='' />
-                </a>
-              </div>
-              <div className='image border-top border-left'>
-                <a
-                  href={data.cube_expansion[4].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <span className='idx'>05</span>
-                  <Image src={CUBE_EXTENSTION_URL(5)} fill alt='' />
-                </a>
-              </div>
-              <div className='text right'>
-                <a
-                  href={data.cube_expansion[3].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <p className='brand'>
-                    {data.cube_expansion[3].cube_expansion_image_name}
-                  </p>
+            <div className='figure'>
+              <div className='block'>
+                <div className='image left'>
+                  <a
+                    href={data.cube_expansion[3].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <span className='idx'>04</span>
+                    <Image src={CUBE_EXTENSTION_URL(4)} fill alt='' />
+                  </a>
+                </div>
+                <div className='image border-top border-left'>
+                  <a
+                    href={data.cube_expansion[4].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <span className='idx'>05</span>
+                    <Image src={CUBE_EXTENSTION_URL(5)} fill alt='' />
+                  </a>
+                </div>
+                <div className='text right'>
+                  <a
+                    href={data.cube_expansion[3].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <p className='brand'>
+                      {data.cube_expansion[3].cube_expansion_image_name}
+                    </p>
 
-                  <p className='product'>
-                    {data.cube_expansion[3].cube_expansion_image_description}
-                  </p>
-                </a>
+                    <p className='product'>
+                      {data.cube_expansion[3].cube_expansion_image_description}
+                    </p>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='figure'>
-            <div className='block'>
-              <div className='text left'>
-                <a
-                  href={data.cube_expansion[4].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <p className='brand'>
-                    {data.cube_expansion[4].cube_expansion_image_name}
-                  </p>
+            <div className='figure'>
+              <div className='block'>
+                <div className='text left'>
+                  <a
+                    href={data.cube_expansion[4].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <p className='brand'>
+                      {data.cube_expansion[4].cube_expansion_image_name}
+                    </p>
 
-                  <p className='product'>
-                    {data.cube_expansion[4].cube_expansion_image_description}
-                  </p>
-                </a>
-              </div>
-              <div className='image border-top'>
-                <a
-                  href={data.cube_expansion[5].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <span className='idx'>06</span>
-                  <Image src={CUBE_EXTENSTION_URL(6)} fill alt='' />
-                </a>
-              </div>
-              <div className='text right'>
-                <a
-                  href={data.cube_expansion[5].cube_expansion_image_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <p className='brand'>
-                    {data.cube_expansion[5].cube_expansion_image_name}
-                  </p>
+                    <p className='product'>
+                      {data.cube_expansion[4].cube_expansion_image_description}
+                    </p>
+                  </a>
+                </div>
+                <div className='image border-top'>
+                  <a
+                    href={data.cube_expansion[5].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <span className='idx'>06</span>
+                    <Image src={CUBE_EXTENSTION_URL(6)} fill alt='' />
+                  </a>
+                </div>
+                <div className='text right'>
+                  <a
+                    href={data.cube_expansion[5].cube_expansion_image_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <p className='brand'>
+                      {data.cube_expansion[5].cube_expansion_image_name}
+                    </p>
 
-                  <p className='product'>
-                    {data.cube_expansion[5].cube_expansion_image_description}
-                  </p>
-                </a>
+                    <p className='product'>
+                      {data.cube_expansion[5].cube_expansion_image_description}
+                    </p>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className='brand-block'>
-        <p>관련 브랜드</p>
-        <div className='brands'>
-          {data.related_brand.brands.map(
-            (
-              { related_brand_image, related_brand_address }: any,
-              idx: number
-            ) => (
-              <div key={idx} className='brand'>
-                <a
-                  href={related_brand_address}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <Image src={BRAND_URL(idx + 1)} alt='' fill />
-                </a>
-              </div>
-            )
-          )}
+        <div className='brand-block'>
+          <p>관련 브랜드</p>
+          <div className='brands'>
+            {data.related_brand.brands.map(
+              (
+                { related_brand_image, related_brand_address }: any,
+                idx: number
+              ) => (
+                <div key={idx} className='brand'>
+                  <a
+                    href={related_brand_address}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    <Image src={BRAND_URL(idx + 1)} alt='' fill />
+                  </a>
+                </div>
+              )
+            )}
+          </div>
         </div>
-      </div>
+      </>
 
       <div className='footer'>
         <div className='share'>
@@ -361,7 +358,7 @@ export default function Page({ params }: { params: { slug: string[] } }) {
       <ToastBar show={isCopied}>링크를 저장했습니다.</ToastBar>
       <ToastBar show={isSaved}>
         <span>
-          <Image width={18} height={18} alt='' src='/images/check-black.svg' />
+          <Image width={12} height={12} alt='' src='/images/check-black.svg' />
         </span>
         이미지 저장 완료
       </ToastBar>

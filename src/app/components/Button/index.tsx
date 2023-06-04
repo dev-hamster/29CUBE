b63971e1c4f0bd2223b76f072572ed928b5bbe52
dndbekfrl1 <123740296+dev-hamster@users.noreply.github.com>
2023-06-05 00:31:50 +0900

    feat: add arrow button

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,22 +1,19 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 import style from './Button.module.css';
 import { formId } from '@/app/constants';
 
-export default function Button({
-  children,
-  isActive,
-  type,
-  handleClick,
-  href,
-}: {
-  children: string;
+interface ButtonProps {
+  children?: string;
   isActive?: boolean;
   type?: 'link' | 'button' | 'submit';
   handleClick?: () => void;
   href?: string;
-}) {
+}
+
+function Button({ children, isActive, type, handleClick, href }: ButtonProps) {
   const themeStyle = style.black;
 
   return (
@@ -36,10 +33,43 @@ export default function Button({
         </button>
       )}
       {type === 'link' && (
-        <div className={`${style.button} ${themeStyle}`}>
-          <Link href={href + ''}>{children}</Link>
-        </div>
+        <Link href={href + ''}>
+          <div className={`${style.button} ${themeStyle}`}>{children}</div>
+        </Link>
       )}
     </>
   );
 }
+
+function NextButton({ isActive, type, handleClick, href }: ButtonProps) {
+  const themeStyle = style.black;
+
+  return (
+    <>
+      {type !== 'link' && (
+        <button
+          form={formId}
+          className={`${style.arrow} ${themeStyle}`}
+          type={type}
+          disabled={!isActive}
+          onClick={(e) => {
+            e.preventDefault();
+            if (handleClick) handleClick();
+          }}
+        >
+          <Image src='/images/arrow.svg' width={20} height={36} alt='' />
+        </button>
+      )}
+      {type === 'link' && (
+        <Link href={href + ''}>
+          <div className={`${style.arrow} ${themeStyle}`}>
+            <Image src='/images/arrow.svg' width={20} height={36} alt='' />
+          </div>
+        </Link>
+      )}
+    </>
+  );
+}
+
+export default Button;
+export { NextButton };
